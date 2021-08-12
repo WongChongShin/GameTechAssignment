2f7f070941c85c11bc6d222d60046c4d7fefeb22
khenghaw <yewkh-wm18@student.tarc.edu.my>
2021-08-12 18:21:36 +0800

    player movement and UI

---
+++ GameTechAssignment/Assets/script/playerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed=0;
+    public Animator anim;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput,0f , verticalInput);
+        direction.Normalize();
+        transform.Translate(direction * speed * Time.deltaTime,Space.World);
+
+        if (direction != Vector3.zero)
+        {
+            transform.forward=direction;
+        }
+        if(Input.GetKey("w")|| Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            anim.SetBool("isRun", true);
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+        }
+    }
+    public void adjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+}
